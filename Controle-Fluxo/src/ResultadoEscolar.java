bb3043a739d71aa5ecb78a700b8a7060c8f97789
Luis Carlos Macêdo Moreira <luiscmacedo84@gmail.com>
2024-06-01 19:25:57 -0300

    Adiciona nova classe ResultadoEscolar e condicionais compostas

---
+++ Controle-Fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,11 @@
+public class ResultadoEscolar {
+    public static void main(String[] args){
+        int nota = 6;
+
+        if(nota >= 7)
+        System.out.print("Aprovado.");
+
+        else
+        System.out.println("Reprovado.");
+    }
+}
