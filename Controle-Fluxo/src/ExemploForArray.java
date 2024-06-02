ee7da34c24eb6cc08f93ade83a73d89a4d67267e
Luis Carlos Macêdo Moreira <luiscmacedo84@gmail.com>
2024-06-02 17:53:12 -0300

    Exemplos de formas de como usar o for dentro de arrays.

---
+++ Controle-Fluxo/src/ExemploForArray.java
@@ -0,0 +1,20 @@
+
+
+public class ExemploForArray {
+    public static void main(String[] args) {
+        
+        String alunos[] = {"Luis","Laura","Vitor"};
+        int numeros[] = {1,2,3,4,5};
+
+        for (String aluno : alunos){
+            System.out.println(aluno);
+        }
+
+        for (int numero = 0;numero<numeros.length;numero++){
+            System.out.println(numero);
+        }
+
+        
+        
+}
+}
