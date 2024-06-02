f6998fb346611beb2b1bbdab1fdab220234f1748
Luis Carlos Macêdo Moreira <luiscmacedo84@gmail.com>
2024-06-02 11:39:29 -0300

    Feat: Adiciona condicional encadeada.

--- Controle-Fluxo/src/ResultadoEscolar.java
+++ Controle-Fluxo/src/ResultadoEscolar.java
@@ -1,9 +1,12 @@
 public class ResultadoEscolar {
     public static void main(String[] args){
-        int nota = 6;
+        int nota = 10;
 
         if(nota >= 7)
         System.out.print("Aprovado.");
+
+        else if(nota>= 5 && nota <7)
+        System.out.println("Prova de recuperação.");
 
         else
         System.out.println("Reprovado.");
